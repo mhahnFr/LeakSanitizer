d3ab46a1a69b22a47547606a5171cd1cdc6a3200
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2022-07-19 19:44:59 +0200

    Added malloc, free and exit replacements

--- wrap_malloc.cpp
+++ wrap_malloc.cpp
@@ -10,12 +10,13 @@
 #include "LeakSani.hpp"
 #include <cstdio>
 #include <iostream>
+#include <execinfo.h>
 
 void * __wrap_malloc(size_t size, const char * file, int line) {
     if (size == 0) {
         crash("Invalid allocation of size 0", file, line);
     }
-    void * ret = std::malloc(size);
+    void * ret = LSan::malloc(size);
     if (ret != nullptr) {
         LSan::getInstance().addMalloc(MallocInfo(ret, size, file, line));
     }
@@ -26,12 +27,46 @@ void __wrap_free(void * pointer, const char * file, int line) {
     if (pointer == nullptr || !LSan::getInstance().removeMalloc(MallocInfo(pointer, 0, file, line))) {
         crash("Invalid free", file, line);
     } else {
-        std::free(pointer);
+        LSan::free(pointer);
     }
 }
 
 [[ noreturn ]] void __wrap_exit(int code, const char * file, int line) {
     std::cout << "\033[32mExiting\033[39m at \033[4m" << file << ":" << line << "\033[24m" << std::endl
               << LSan::getInstance() << std::endl;
-    std::exit(code);
+    LSan::exit(code);
 }
+
+void * malloc(size_t size) {
+    if (size == 0) {
+        crash("Invalid allocation of size 0");
+    }
+    char ** buf = new char*[1000]();
+    backtrace((void**) buf, 1000);
+    char ** bt = backtrace_symbols((void**) buf, 1000);
+    void * ptr = LSan::malloc(size);
+    LSan::getInstance().addMalloc(MallocInfo(ptr, size, std::string(bt[0]), 1));
+    return ptr;
+}
+
+void free(void * pointer) {
+    if (pointer == nullptr || !LSan::getInstance().removeMalloc(MallocInfo(pointer, 0))) {
+        crash("Invalid free");
+    } else {
+        LSan::free(pointer);
+    }
+}
+
+void exit(int code) {
+    std::cout << "\033[32mExiting\033[39m" << std::endl
+              << LSan::getInstance() << std::endl;
+    LSan::exit(code);
+}
+//
+//#define DYLD_INTERPOSE(_replacement,_replacee) \
+//   __attribute__((used)) static struct{ const void* replacement; const void* replacee; } _interpose_##_replacee \
+//            __attribute__ ((section ("__DATA,__interpose"))) = { (const void*)(unsigned long)&_replacement, (const void*)(unsigned long)&_replacee };
+//
+//DYLD_INTERPOSE(Aexit, exit)
+//DYLD_INTERPOSE(Amalloc, malloc)
+//DYLD_INTERPOSE(Afree, free)
