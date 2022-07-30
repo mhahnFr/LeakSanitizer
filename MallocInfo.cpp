13d382b578e8e984b0e1765a57bdc71aff1d207f
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2022-07-30 20:14:51 +0200

    Added call stacks to the MallocInfo class

--- MallocInfo.cpp
+++ MallocInfo.cpp
@@ -7,9 +7,34 @@
 //
 
 #include "MallocInfo.hpp"
+#include <execinfo.h>
+#include <dlfcn.h>
+#include <cxxabi.h>
 
 MallocInfo::MallocInfo(const void * const pointer, size_t size, const std::string & file, const int line)
-    : pointer(pointer), size(size), createdInFile(file), createdOnLine(line) {}
+    : pointer(pointer), size(size), createdInFile(file), createdOnLine(line), createdCallstack(createCallstack()) {}
+
+const std::vector<std::string> MallocInfo::createCallstack() {
+    std::vector<std::string> ret;
+    void * callstack[128];
+    int frames = backtrace(callstack, 128);
+    char ** symbols = backtrace_symbols(callstack, frames);
+
+    for (int i = 3; i < 128; ++i) {
+        Dl_info info;
+        if (dladdr(callstack[i], &info)) {
+            char * demangled;
+            int status;
+            if ((demangled = abi::__cxa_demangle(info.dli_sname, nullptr, 0, &status)) != nullptr) {
+                //message += std::string(demangled) + ": " + std::to_string((char *)callstack[i] - (char *)info.dli_saddr) + "\n";
+                ret.push_back(demangled);
+                free(demangled);
+            }
+        }
+    }
+    free(symbols);
+    return ret;
+}
 
 const void * const MallocInfo::getPointer() const {
     return pointer;
@@ -41,6 +66,30 @@ void MallocInfo::setDeletedInFile(const std::string & file) {
 
 void MallocInfo::setDeletedOnLine(int line) {
     deletedOnLine = line;
+}
+
+void MallocInfo::generateDeletedCallstack() {
+    deletedCallstack = createCallstack();
+}
+
+void MallocInfo::printCreatedCallstack(std::ostream & stream) const {
+    for (const auto & frame : createdCallstack) {
+        stream << frame << std::endl;
+    }
+}
+
+void MallocInfo::printDeletedCallstack(std::ostream & stream) const {
+    for (const auto & frame : deletedCallstack) {
+        stream << frame << std::endl;
+    }
+}
+
+const std::vector<std::string> & MallocInfo::getCreatedCallstack() const {
+    return createdCallstack;
+}
+
+const std::vector<std::string> & MallocInfo::getDeletedCallstack() const {
+    return deletedCallstack;
 }
 
 bool operator==(const MallocInfo & lhs, const MallocInfo & rhs) {
--- MallocInfo.hpp
+++ MallocInfo.hpp
@@ -9,20 +9,27 @@
 #ifndef MallocInfo_hpp
 #define MallocInfo_hpp
 
+#include <vector>
+#include <ostream>
 #include <string>
 #include <utility>
 
 using namespace std::rel_ops;
 
 class MallocInfo {
-    const void * const pointer;
-    const size_t       size;
-    const std::string  createdInFile;
-    const int          createdOnLine;
+    constexpr static int CALLSTACK_SIZE = 128;
     
-    std::string        deletedInFile;
-    int                deletedOnLine;
+    const void * const              pointer;
+    const size_t                    size;
     
+    const std::string               createdInFile;
+    const int                       createdOnLine;
+    const std::vector<std::string>  createdCallstack;
+
+    std::string                     deletedInFile;
+    int                             deletedOnLine;
+    std::vector<std::string>        deletedCallstack;
+        
 public:
     MallocInfo(const void * const pointer, size_t size): MallocInfo(pointer, size, "<Unknown>", 1) {}
     MallocInfo(const void * const, size_t, const std::string &, int);
@@ -38,6 +45,15 @@ public:
     
     int                 getDeletedOnLine()  const;
     void                setDeletedOnLine(int);
+    void                generateDeletedCallstack();
+    
+    void                printCreatedCallstack(std::ostream &) const;
+    void                printDeletedCallstack(std::ostream &) const;
+    
+    const std::vector<std::string> & getDeletedCallstack() const;
+    const std::vector<std::string> & getCreatedCallstack() const;
+
+    static const std::vector<std::string> createCallstack();
     
     friend bool operator==(const MallocInfo &, const MallocInfo &);
     friend bool operator<(const MallocInfo &, const MallocInfo &);
