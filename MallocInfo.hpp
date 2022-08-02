88735778156c28932bbb10dd1a211dcad93f9322
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2022-08-02 14:37:33 +0200

    Cleaned up the MallocInfo header

--- MallocInfo.hpp
+++ MallocInfo.hpp
@@ -21,49 +21,47 @@ class MallocInfo {
     
     MallocInfo(const void * const, size_t, const std::string &, int, int, bool);
     
-    const void * const          pointer;
-    const size_t                size;
+    const void * const       pointer;
+    const size_t             size;
     
-    const std::string           createdInFile;
-    const int                   createdOnLine;
-    const bool                  createdSet;
-    std::vector<std::string>    createdCallstack;
+    const std::string        createdInFile;
+    const int                createdOnLine;
+    const bool               createdSet;
+    std::vector<std::string> createdCallstack;
 
-    std::string                 deletedInFile;
-    int                         deletedOnLine;
-    std::vector<std::string>    deletedCallstack;
-    
-    static void printCallstack(const std::vector<std::string> &, std::ostream &);
+    std::string              deletedInFile;
+    int                      deletedOnLine;
+    std::vector<std::string> deletedCallstack;
     
 public:
     MallocInfo(const void * const, size_t, int = 4);
     MallocInfo(const void * const, size_t, const std::string &, int, int = 4);
     ~MallocInfo() = default;
     
-    const void * const  getPointer()        const;
-    const std::string & getCreatedInFile()  const;
-    int                 getCreatedOnLine()  const;
-    size_t              getSize()           const;
+    auto getPointer()        const -> const void * const;
+    auto getCreatedInFile()  const -> const std::string &;
+    auto getCreatedOnLine()  const -> int;
+    auto getSize()           const -> size_t;
     
-    const std::string & getDeletedInFile()  const;
-    void                setDeletedInFile(const std::string &);
+    void setDeletedInFile(const std::string &);
+    auto getDeletedInFile()  const -> const std::string &;
     
-    int                 getDeletedOnLine()  const;
-    void                setDeletedOnLine(int);
-    void                generateDeletedCallstack();
+    void setDeletedOnLine(int);
+    void generateDeletedCallstack();
+    auto getDeletedOnLine()  const -> int;
     
-    void                printCreatedCallstack(std::ostream &) const;
-    void                printDeletedCallstack(std::ostream &) const;
+    void printCreatedCallstack(std::ostream &) const;
+    void printDeletedCallstack(std::ostream &) const;
     
-    const std::vector<std::string> & getDeletedCallstack() const;
-    const std::vector<std::string> & getCreatedCallstack() const;
+    auto getDeletedCallstack() const -> const std::vector<std::string> &;
+    auto getCreatedCallstack() const -> const std::vector<std::string> &;
 
-    static const std::vector<std::string> createCallstack(int = 1);
+    static void printCallstack(const std::vector<std::string> &, std::ostream &);
+    static auto createCallstack(int = 1) -> const std::vector<std::string>;
     
-    friend bool operator==(const MallocInfo &, const MallocInfo &);
-    friend bool operator<(const MallocInfo &, const MallocInfo &);
-    
-    friend std::ostream & operator<<(std::ostream &, const MallocInfo &);
+    friend auto operator==(const MallocInfo &, const MallocInfo &) -> bool;
+    friend auto operator<(const MallocInfo &, const MallocInfo &)  -> bool;
+    friend auto operator<<(std::ostream &, const MallocInfo &)     -> std::ostream &;
 };
 
 #endif /* MallocInfo_hpp */
