6c7a1b2b72df8f6c77261d700bb2d40a941c545a
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2022-08-03 17:08:11 +0200

    Added missing license notices and fixes to it

--- warn.cpp
+++ warn.cpp
@@ -1,10 +1,21 @@
-//
-//  warn.cpp
-//  LeakSanitizer
-//
-//  Created by Manuel Hahn on 8/3/22.
-//  Copyright © 2022 mhahnFr. All rights reserved.
-//
+/*
+ * LeakSanitizer - A small library showing informations about lost memory.
+ *
+ * Copyright (C) 2022  mhahnFr
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
 
 #include <iostream>
 #include "warn.hpp"
