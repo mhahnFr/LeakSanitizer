7b506ba9639e41e7a42b7961ee57dc510f3a5e0f
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2022-08-06 20:55:50 +0200

    Added basic stats printing

--- lsan_stats.cpp
+++ lsan_stats.cpp
@@ -51,6 +51,21 @@ size_t __lsan_getBytePeek() {
 
 void __lsan_printStats() {
     LSan::setIgnoreMalloc(true);
-    
+    std::cout << "Stats of the memory usage so far: " << std::endl
+              << __lsan_getCurrentMallocCount() << " objects in the heap, peek " << __lsan_getMallocPeek() << ", " << __lsan_getTotalFrees() << " deleted objects." << std::endl << std::endl
+              << __lsan_getCurrentByteCount() << " bytes currently used, peek " << __lsan_getBytePeek() << " bytes." << std::endl;
+    std::cout << "[";
+    size_t i;
+    for (i = 0; i < (static_cast<float>(__lsan_getCurrentByteCount()) / __lsan_getTotalBytes()) * 100; ++i) {
+        std::cout << '*';
+    }
+    for (; i < (static_cast<float>(__lsan_getBytePeek()) / __lsan_getTotalBytes()) * 100; ++i) {
+        std::cout << ' ';
+    }
+    std::cout << '|';
+    for (; i < 100; ++i) {
+        std::cout << ' ';
+    }
+    std::cout << "] of " << __lsan_getTotalBytes() << " total bytes" << std::endl;
     LSan::setIgnoreMalloc(false);
 }
