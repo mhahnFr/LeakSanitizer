3cc1162f7c53dab7ae9a25087405af718aae8f0d
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2022-08-06 19:40:37 +0200

    Added stats

---
+++ Stats.cpp
@@ -0,0 +1,81 @@
+/*
+ * LeakSanitizer - A small library showing informations about lost memory.
+ *
+ * Copyright (C) 2022  mhahnFr
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+
+#include "Stats.hpp"
+
+size_t Stats::getCurrentMallocCount() const { return currentMallocCount; }
+size_t Stats::getTotalMallocCount()   const { return totalMallocCount;   }
+size_t Stats::getMallocPeek()         const { return peekMallocCount;    }
+
+size_t Stats::getCurrentBytes() const { return currentBytes; }
+size_t Stats::getTotalBytes()   const { return totalBytes;   }
+size_t Stats::getBytePeek()     const { return peekBytes;    }
+
+size_t Stats::getTotalFreeCount() const { return freeCount; }
+
+void Stats::addMalloc(size_t size) {
+    ++currentMallocCount;
+    ++totalMallocCount;
+    if (peekMallocCount < currentMallocCount) {
+        peekMallocCount = currentMallocCount;
+    }
+    
+    currentBytes += size;
+    totalBytes   += size;
+    if (peekBytes < currentBytes) {
+        peekBytes = currentBytes;
+    }
+}
+
+void Stats::addMalloc(const MallocInfo & mInfo) {
+    addMalloc(mInfo.getSize());
+}
+
+void Stats::addFree(size_t size) {
+    ++freeCount;
+    
+    --currentMallocCount;
+    currentBytes -= size;
+}
+
+void Stats::addFree(const MallocInfo & mInfo) {
+    addFree(mInfo.getSize());
+}
+
+Stats Stats::operator+(const MallocInfo & minfo) {
+    Stats old = *this;
+    addMalloc(minfo);
+    return old;
+}
+
+Stats & Stats::operator+=(const MallocInfo & mInfo) {
+    addMalloc(mInfo);
+    return *this;
+}
+
+Stats Stats::operator-(const MallocInfo & mInfo) {
+    Stats old = *this;
+    addFree(mInfo);
+    return old;
+}
+
+Stats & Stats::operator-=(const MallocInfo & mInfo) {
+    addFree(mInfo);
+    return *this;
+}
---
+++ Stats.hpp
@@ -0,0 +1,61 @@
+/*
+ * LeakSanitizer - A small library showing informations about lost memory.
+ *
+ * Copyright (C) 2022  mhahnFr
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/.
+ */
+
+#ifndef Stats_hpp
+#define Stats_hpp
+
+#include <cstddef>
+#include "MallocInfo.hpp"
+
+class Stats {
+    size_t currentMallocCount,
+             totalMallocCount,
+              peekMallocCount;
+
+    size_t currentBytes,
+             totalBytes,
+              peekBytes;
+    
+    size_t freeCount;
+    
+public:
+    auto getCurrentMallocCount() const -> size_t;
+    auto getTotalMallocCount()   const -> size_t;
+    auto getMallocPeek()         const -> size_t;
+    
+    auto getCurrentBytes() const -> size_t;
+    auto getTotalBytes()   const -> size_t;
+    auto getBytePeek()     const -> size_t;
+    
+    auto getTotalFreeCount() const -> size_t;
+    
+    void addMalloc(size_t size);
+    void addMalloc(const MallocInfo &);
+    
+    void addFree(size_t size);
+    void addFree(const MallocInfo &);
+    
+    auto operator+ (const MallocInfo &) -> Stats;
+    auto operator+=(const MallocInfo &) -> Stats &;
+    
+    auto operator- (const MallocInfo &) -> Stats;
+    auto operator-=(const MallocInfo &) -> Stats &;
+};
+
+#endif /* Stats_hpp */
