da1347d8e017e3163ee46629c7a71a57861d8c9a
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2022-09-09 18:30:06 +0200

    Implemented island printing for the byte fragmentation as well

--- code/lsan_stats.cpp
+++ code/lsan_stats.cpp
@@ -193,6 +193,8 @@ static inline void __lsan_printFragmentationByteBar(size_t width, std::ostream &
         const float step = total / static_cast<float>(width),
                     loss = fmod(step, static_cast<int>(step));
         float    tmpLoss = 0.0f;
+        bool previousFilled = false;
+        size_t previousFs = 0;
         for (size_t i = 0; i < width; ++i) {
             size_t tmpStep = step;
             tmpLoss += loss;
@@ -211,7 +213,22 @@ static inline void __lsan_printFragmentationByteBar(size_t width, std::ostream &
                     ++fs;
                 }
             }
-            out << ((fs >= step / 2.0f) ? Formatter::get(Style::BAR_EMPTY) : Formatter::get(Style::BAR_FILLED));
+            if (!previousFilled && fs < previousFs) {
+                out << Formatter::get(Style::BAR_FILLED);
+                previousFilled = true;
+            } else if (fs < step / 2.0f) {
+                if (previousFilled && fs > previousFs) {
+                    out << Formatter::get(Style::BAR_EMPTY);
+                    previousFilled = false;
+                } else {
+                    out << Formatter::get(Style::BAR_FILLED);
+                    previousFilled = true;
+                }
+            } else {
+                out << Formatter::get(Style::BAR_EMPTY);
+                previousFilled = false;
+            }
+            previousFs = fs;
         }
     }
     out << Formatter::clear(Style::GREYED) << Formatter::clear(Style::UNDERLINED)
