0cef70f4e73133ea0f8b594e06a433deb85456a4
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2022-11-10 19:50:55 +0100

    Documented the allocation record

--- code/MallocInfo.hpp
+++ code/MallocInfo.hpp
@@ -29,50 +29,178 @@
 
 using namespace std::rel_ops;
 
+/**
+ * @brief This class acts as a allocation record: all information about the allocation process
+ * that is available is stored.
+ *
+ * It features a callstack and the file name and the line number of the allocation. The size and the
+ * pointer are stored as well.
+ */
 class MallocInfo {
-    MallocInfo(void * const, size_t, const std::string &, int, void *, bool);
+    /**
+     * Initializes this allocation record using the given information.
+     *
+     * @param pointer the pointer to the allocated piece of memory
+     * @param size the size of the allocated piece of memory
+     * @param file the filename where the allocation happened
+     * @param line the line number inside the file where the alocation happened
+     * @param omitAddress the return address upon which function calls are ignored
+     * @param createdSet whether the file and line information are actually set
+     */
+    MallocInfo(void * const pointer, size_t size, const std::string & file, int line, void * omitAddress, bool createdSet);
     
-    void *      pointer;
-    size_t      size;
+    /// The pointer to the allocated piece of memory.
+    void * pointer;
+    /// The size of the allocated piece of memory.
+    size_t size;
     
+    /// The filename in which this allocation happened.
     std::string            createdInFile;
+    /// The line number in which this allocation happened.
     int                    createdOnLine;
+    /// Indicating whether the file and the line information have been set.
     bool                   createdSet;
+    /// The callstack where this allocation happened.
     mutable lcs::callstack createdCallstack;
 
+    /// The filename in which this allocation was deallocated.
     std::string            deletedInFile;
+    /// The line number in which this allocation was deallocated.
     int                    deletedOnLine;
+    /// Indicating whether this allocation has been deallocated.
     bool                   deleted;
+    /// The callstack where the deallocation happened.
     mutable lcs::callstack deletedCallstack;
         
 public:
-    MallocInfo(void * const, size_t, void * = __builtin_return_address(0));
-    MallocInfo(void * const, size_t, const std::string &, int, void * = __builtin_return_address(0));
+    /**
+     * Initializes this allocation record using the given information.
+     *
+     * @param pointer the pointer to the allocated piece of memory
+     * @param size the size of the allocated piece of memory
+     * @param omitAddress the return address upon which function calls are ignored
+     */
+    MallocInfo(void * const pointer, size_t size, void * omitAddress = __builtin_return_address(0));
+    /**
+     * Initializes this allocation record using the given information.
+     *
+     * @param pointer the pointer to the allocated piece of memory
+     * @param size the size of the allocated piece of memory
+     * @param file the filename where the allocation happened
+     * @param line the line number inside the file where the allocation happened
+     * @param omitAddress the return address upon which function calls are ignored
+     */
+    MallocInfo(void * const pointer, size_t size, const std::string & file, int line, void * omitAddress = __builtin_return_address(0));
     
+    /**
+     * Returns the pointer to the allocated piece of memory.
+     *
+     * @return the pointer to the allocated memory
+     */
     auto getPointer()        const -> const void *;
+    /**
+     * @brief Returns the filename where the allocation happened.
+     *
+     * @return the filename where the allocation happened.
+     */
     auto getCreatedInFile()  const -> const std::string &;
+    /**
+     * @brief Returns the line number where the allocation happend.
+     *
+     * @return the line number where the allocation happend
+     */
     auto getCreatedOnLine()  const -> int;
+    /**
+     * Returns the size of the allocated piece of memory.
+     *
+     * @return the size of the alocated memory block
+     */
     auto getSize()           const -> size_t;
     
-    void setDeletedInFile(const std::string &);
+    /**
+     * Sets the filename in which this allocation was deallocated.
+     *
+     * @param file the filename
+     */
+    void setDeletedInFile(const std::string & file);
+    /**
+     * Returns the filename where this allocation was deallocated.
+     *
+     * @return the filename where the deallocation happened
+     */
     auto getDeletedInFile()  const -> const std::string &;
     
-    void setDeletedOnLine(int);
+    /**
+     * Sets the line number where this allocation was deallocated.
+     *
+     * @param line the line number
+     */
+    void setDeletedOnLine(int line);
+    /**
+     * Returns the line number where this allocation was deallocated.
+     *
+     * @return the line number where the deallocation happened
+     */
     auto getDeletedOnLine()  const -> int;
     
+    /**
+     * Returns whether this allocation has been deallocated.
+     *
+     * @return whether this allocation is marked as deallocated
+     */
     auto isDeleted()         const -> bool;
-    void setDeleted(bool);
+    /**
+     * Sets whether this alloction has been deallocated.
+     *
+     * @param deleted whether this allocation is deallocated
+     */
+    void setDeleted(bool deleted);
     
+    /**
+     * Generates and stores a callstack for the deallocation callstack.
+     */
     void generateDeletedCallstack();
     
-    void printCreatedCallstack(std::ostream &) const;
-    void printDeletedCallstack(std::ostream &) const;
+    /**
+     * Prints the callstack where this allocation happened.
+     *
+     * @param out the output stream to print to
+     */
+    void printCreatedCallstack(std::ostream & out) const;
+    /**
+     * Prints the callstack where this allocation was deallocated.
+     *
+     * @param out the output stream to print to
+     */
+    void printDeletedCallstack(std::ostream & out) const;
     
+    /**
+     * Returns a reference to the callstack where this allocation was deallocated.
+     *
+     * @return the callstack where the deallocation happened
+     */
     auto getDeletedCallstack() const -> const lcs::callstack &;
+    /**
+     * Returns a reference to the callstack where this allocation was allocated.
+     *
+     * @return the callstack where the allocation happened
+     */
     auto getCreatedCallstack() const -> const lcs::callstack &;
 
-    static void printCallstack(lcs::callstack &,  std::ostream &);
-    static void printCallstack(lcs::callstack &&, std::ostream &);
+    /**
+     * Prints the given callstack formatted on the given output stream.
+     *
+     * @param callstack the callstack to be printed
+     * @param out the output stream to print to
+     */
+    static void printCallstack(lcs::callstack & callstack,  std::ostream & out);
+    /**
+     * Prints the given callstack formatted on the given output stream.
+     *
+     * @param callstack the callstack to be printed
+     * @param out the output stream to print to
+     */
+    static void printCallstack(lcs::callstack && callstack, std::ostream & out);
     
     friend auto operator==(const MallocInfo &, const MallocInfo &) -> bool;
     friend auto operator<(const MallocInfo &, const MallocInfo &)  -> bool;
