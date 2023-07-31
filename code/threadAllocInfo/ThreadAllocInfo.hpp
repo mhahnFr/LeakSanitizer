dbdee7fa667b555d1174e46889b952cc81d3bdf7
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-07-31 18:29:22 +0200

    Added thread local sanitizer class

---
+++ code/threadAllocInfo/ThreadAllocInfo.hpp
@@ -0,0 +1,45 @@
+/*
+ * LeakSanitizer - Small library showing information about lost memory.
+ *
+ * Copyright (C) 2023  mhahnFr
+ *
+ * This file is part of the LeakSanitizer. This library is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this library, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef ThreadAllocInfo_hpp
+#define ThreadAllocInfo_hpp
+
+#include <map>
+#include <mutex>
+
+#include "../MallocInfo.hpp"
+#include "../Stats.hpp"
+
+class ThreadAllocInfo {
+    Stats stats;
+    std::recursive_mutex statsMutex;
+    std::map<const void * const, MallocInfo> infos;
+    
+public:
+    ThreadAllocInfo();
+   ~ThreadAllocInfo();
+    
+    ThreadAllocInfo(const ThreadAllocInfo &) = delete;
+    ThreadAllocInfo(ThreadAllocInfo &&)      = delete;
+    
+    ThreadAllocInfo & operator=(const ThreadAllocInfo &) = delete;
+    ThreadAllocInfo & operator=(ThreadAllocInfo &&)      = delete;
+};
+
+#endif /* ThreadAllocInfo_hpp */
