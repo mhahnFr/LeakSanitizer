d86d1480f56e81d0b8ab15557943dc0edb61441f
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-08-19 17:24:50 +0200

    Added conversion functions

--- code/lsan_internals.cpp
+++ code/lsan_internals.cpp
@@ -17,6 +17,10 @@
  * this library, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <cstdlib>
+#include <optional>
+#include <string>
+
 #include "../include/lsan_internals.h"
 
 bool   __lsan_humanPrint       = true;
@@ -48,3 +52,51 @@ bool   __lsan_printStatsOnExit = false;
 size_t __lsan_leakCount        = 100;
 
 size_t __lsan_callstackSize    = 20;
+
+
+static inline std::optional<std::string> getVariable(const std::string & name) {
+    const char * var = getenv(name.c_str());
+    
+    if (var == nullptr) {
+        return std::nullopt;
+    }
+    std::string s = var;
+    for (auto it = s.begin(); it != s.end(); ++it) {
+        *it = tolower(*it);
+    }
+    return s;
+}
+
+static inline std::optional<bool> getBool(const std::string & name) {
+    auto var = getVariable(name);
+    if (!var.has_value()) {
+        return std::nullopt;
+    }
+    
+    auto s = var.value();
+    if (s == "true" || s == "1") {
+        return true;
+    } else if (s == "false" || s == "0") {
+        return false;
+    }
+    return std::nullopt;
+}
+
+static inline std::optional<std::size_t> getSize_t(const std::string & name) {
+    auto var = getVariable(name);
+    
+    if (!var.has_value()) {
+        return std::nullopt;
+    }
+    
+    auto s = var.value();
+    try {
+        return stoll(s);
+    } catch (const std::exception &) {
+        return std::nullopt;
+    }
+}
+
+void initInternals() {
+    // TODO: Implement
+}
