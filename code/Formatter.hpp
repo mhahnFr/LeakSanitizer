cc6c178c7b5e723a9118099b860f28b9276f78ef
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-09-15 22:57:18 +0200

    Replaced the formatting functions by templates

--- code/Formatter.hpp
+++ code/Formatter.hpp
@@ -1,7 +1,7 @@
 /*
- * LeakSanitizer - A small library showing informations about lost memory.
+ * LeakSanitizer - Small library showing information about lost memory.
  *
- * Copyright (C) 2022  mhahnFr and contributors
+ * Copyright (C) 2022 - 2023  mhahnFr and contributors
  *
  * This file is part of the LeakSanitizer. This library is free software:
  * you can redistribute it and/or modify it under the terms of the
@@ -21,6 +21,8 @@
 #define Formatter_hpp
 
 #include <string>
+
+#include "../include/lsan_internals.h"
 
 /**
  * A namespace containing helper functions and classes for formatting
@@ -49,27 +51,70 @@ namespace Formatter {
         BAR_EMPTY
     };
     
-    /**
-     * @brief Returns an ANSI escape code for the requested style.
-     *
-     * The returned string might be empty if the `__lsan_printFormatted` is
-     * set to `false`.
-     *
-     * @param style the requested style
-     * @return the corresponding escape code
-     */
-    auto get(Style style)   -> std::string;
+/**
+ * @brief Returns an ANSI escape code for the requested style.
+ *
+ * The returned string might be empty if `__lsan_printFormatted` is
+ * set to `false`.
+ *
+ * @return the corresponding escape code
+ * @tparam S the requested style
+ */
+template<Style S>
+constexpr inline auto get() -> const char * {
+    if (!__lsan_printFormatted) {
+        switch (S) {
+            case Style::BAR_EMPTY:  return ".";
+            case Style::BAR_FILLED: return "=";
+            default:
+                return "";
+        }
+    }
+    switch (S) {
+        case Style::BAR_EMPTY:  return " ";
+        case Style::BAR_FILLED: return "*";
+        case Style::BOLD:       return "\033[1m";
+        case Style::GREEN:      return "\033[32m";
+        case Style::GREYED:     return "\033[2m";
+        case Style::ITALIC:     return "\033[3m";
+        case Style::MAGENTA:    return "\033[95m";
+        case Style::RED:        return "\033[31m";
+        case Style::UNDERLINED: return "\033[4m";
+        default:
+            return "";
+    }
+}
 
-    /**
-     * @brief Returns an ANSI esape code to clear the given style.
-     *
-     * The returned string might be empty if the `__lsan_printFormatted` is
-     * set to `false`.
-     *
-     * @param style the style to clear
-     * @return the corresponding escape code
-     */
-    auto clear(Style style) -> std::string;
+/**
+ * @brief Returns an ANSI esape code to clear the given style.
+ *
+ * The returned string might be empty if `__lsan_printFormatted` is
+ * set to `false`.
+ *
+ * @return the corresponding escape code
+ * @tparam S the style to clear
+ */
+template<Style S>
+constexpr inline auto clear() -> const char * {
+    if (!__lsan_printFormatted) {
+        return "";
+    }
+    switch (S) {
+        case Style::RED:
+        case Style::GREEN:
+        case Style::MAGENTA:    return "\033[39m";
+            
+        case Style::BOLD:
+        case Style::GREYED:     return "\033[22m";
+            
+        case Style::ITALIC:     return "\033[23m";
+            
+        case Style::UNDERLINED: return "\033[24m";
+            
+        default:
+            return "";
+    }
+}
 
     /**
      * Returns an ANSI escape code to clear all possible styles.
