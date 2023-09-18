9cd9c3deaa982ca155906734f3943ec006e11d34
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-09-18 19:21:48 +0200

    Added library initializer

---
+++ code/initialization/init.cpp
@@ -0,0 +1,31 @@
+/*
+ * LeakSanitizer - Small library showing information about lost memory.
+ *
+ * Copyright (C) 2023  mhahnFr
+ *
+ * This file is part of the LeakSanitizer. This library is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this library, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "init.h"
+
+#include "../LeakSani.hpp"
+
+bool inited = false;
+
+void onLoad() {
+    (void) LSan::getIgnoreMalloc();
+    (void) LSan::getInstance();
+    
+    inited = true;
+}
---
+++ code/initialization/init.h
@@ -0,0 +1,37 @@
+/*
+ * LeakSanitizer - Small library showing information about lost memory.
+ *
+ * Copyright (C) 2023  mhahnFr
+ *
+ * This file is part of the LeakSanitizer. This library is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this library, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef init_h
+#define init_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdbool.h>
+
+extern bool inited;
+
+void onLoad(void) __attribute__((constructor));
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif /* init_h */
