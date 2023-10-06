0fd031f3b49b71f037b32e450d25ac54eef8fccb
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-10-06 19:42:41 +0200

    Documented the callstack helper

--- src/callstacks/callstackHelper.hpp
+++ src/callstacks/callstackHelper.hpp
@@ -25,18 +25,42 @@
 
 #include "../../CallstackLibrary/include/callstack.h"
 
+/** This namespace includes the helper functions for the callstacks. */
 namespace lsan::callstackHelper {
+/** An enumeration containing the possible callstack types.  */
 enum class CallstackType {
+    /** Indicates the callstack shoud be ignored completely. */
     HARD_IGNORE,
+    /** Indicates the callstack is entirely first party.     */
     FIRST_PARTY,
+    /** Indicates the callstack originates in first party.   */
     FIRST_PARTY_ORIGIN,
+    /** Indicates the callstack is user relevant.            */
     USER
 };
 
+/**
+ * Returns the type of the given callstack.
+ *
+ * @param callstack the callstack
+ * @return the type of the callstack
+ */
 auto getCallstackType(lcs::callstack & callstack) -> CallstackType;
 
+/**
+ * Formats the given callstack onto the given output stream.
+ *
+ * @param callstack the callstack
+ * @param stream the stream to print to
+ */
 void format(lcs::callstack & callstack, std::ostream & stream);
 
+/**
+ * Formats the given callstack onto the given output stream.
+ *
+ * @param callstack the callstack
+ * @param out the stream to print to
+ */
 static inline void format(lcs::callstack && callstack, std::ostream & out) {
     format(callstack, out);
 }
