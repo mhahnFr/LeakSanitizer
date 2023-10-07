435a2e505a0bf369a5b76cd635a52fdb0a986fcd
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-10-07 18:34:19 +0200

    Added a new implementation of the static helper functions

---
+++ src/lsanMisc.cpp
@@ -0,0 +1,92 @@
+/*
+ * LeakSanitizer - Small library showing information about lost memory.
+ *
+ * Copyright (C) 2023  mhahnFr
+ *
+ * This file is part of the LeakSanitizer. This library is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this library, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+
+#include "lsanMisc.hpp"
+
+#include "formatter.hpp"
+#include "callstacks/callstackHelper.hpp"
+
+#include "../include/lsan_internals.h"
+#include "../CallstackLibrary/include/callstack.h"
+
+namespace lsan {
+auto _getIgnoreMalloc() -> bool & {
+    static bool ignore = false;
+    return ignore;
+}
+
+auto getInstance() -> LSan & {
+    static auto instance = new LSan();
+    return *instance;
+}
+
+/**
+ * Prints the license information of this sanitizer.
+ */
+static inline void printLicense() {
+    std::ostream & out = __lsan_printCout ? std::cout : std::cerr;
+    out << "Copyright (C) 2022 - 2023  mhahnFr and contributors" << std::endl
+        << "Licensed under the terms of the GPL 3.0."            << std::endl
+        << std::endl;
+}
+
+/**
+ * Prints the link to the website of this sanitizer.
+ */
+static inline void printWebsite() {
+    using formatter::Style;
+    
+    std::ostream & out = __lsan_printCout ? std::cout : std::cerr;
+    out << formatter::get<Style::ITALIC>
+        << "For more information, visit "
+        << formatter::format<Style::UNDERLINED>("github.com/mhahnFr/LeakSanitizer")
+        << formatter::clear<Style::ITALIC>
+        << std::endl << std::endl;
+}
+
+void printInformation() {
+    using formatter::Style;
+    
+    std::ostream & out = __lsan_printCout ? std::cout : std::cerr;
+    out << "Report by " << formatter::format<Style::BOLD>("LeakSanitizer ")
+    << formatter::format<Style::ITALIC>(VERSION)
+    << std::endl << std::endl;
+    if (__lsan_printLicense) printLicense();
+    if (__lsan_printWebsite) printWebsite();
+}
+
+void exitHook() {
+    using formatter::Style;
+    
+    setIgnoreMalloc(true);
+    std::ostream & out = __lsan_printCout ? std::cout : std::cerr;
+    out << std::endl << formatter::format<Style::GREEN>("Exiting");
+    
+    if (__lsan_printExitPoint) {
+        out << formatter::format<Style::ITALIC>(", stacktrace:") << std::endl;
+        callstackHelper::format(lcs::callstack(__builtin_return_address(0)), out);
+    }
+    out << std::endl     << std::endl
+        << getInstance() << std::endl;
+    printInformation();
+    internalCleanUp();
+}
+}
---
+++ src/lsanMisc.hpp
@@ -0,0 +1,88 @@
+/*
+ * LeakSanitizer - Small library showing information about lost memory.
+ *
+ * Copyright (C) 2023  mhahnFr
+ *
+ * This file is part of the LeakSanitizer. This library is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this library, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef lsanMisc_hpp
+#define lsanMisc_hpp
+
+#include "LeakSani.hpp"
+
+namespace lsan {
+/**
+ * Returns a reference to a boolean value indicating
+ * whether to ignore allocations.
+ *
+ * @return the ignoration flag
+ */
+auto _getIgnoreMalloc() -> bool &;
+
+/**
+ * Returns the current instance of this sanitizer.
+ *
+ * @return the current instance
+ */
+auto getInstance() -> LSan &;
+
+/**
+ * Prints the additional information about this sanitizer.
+ */
+void printInformation();
+
+/**
+ * @brief The hook to be called on exit.
+ *
+ * It prints all informations tracked by the sanitizer and performs internal cleaning.
+ */
+void exitHook();
+
+/**
+ * Sets whether to ignore subsequent allocation management requests.
+ *
+ * @param ignoreMalloc whether to ignore allocations
+ */
+static inline void setIgnoreMalloc(const bool ignoreMalloc) {
+    _getIgnoreMalloc() = ignoreMalloc;
+}
+
+/**
+ * Returns whether to ignore subsequent alloction management requests.
+ *
+ * @return whether to ignore allocations
+ */
+static inline auto getIgnoreMalloc() -> bool {
+    return _getIgnoreMalloc();
+}
+
+/**
+ * Returns the current instance of the statistics object.
+ *
+ * @return the current statistics instance
+ */
+static inline auto getStats() -> const Stats & {
+    return getInstance().getStats();
+}
+
+/**
+ * Deletes the currently active instance of the sanitizer.
+ */
+static inline void internalCleanUp() {
+    delete std::addressof(getInstance());
+}
+}
+
+#endif /* lsanMisc_hpp */
