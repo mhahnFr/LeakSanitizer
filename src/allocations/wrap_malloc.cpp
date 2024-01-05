500b9fdfc8a42429079575e9218e699300ec0eee
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-01-05 15:49:16 +0100

    Corrected copyright notices

--- src/allocations/wrap_malloc.cpp
+++ src/allocations/wrap_malloc.cpp
@@ -1,7 +1,7 @@
 /*
  * LeakSanitizer - Small library showing information about lost memory.
  *
- * Copyright (C) 2022 - 2023  mhahnFr and contributors
+ * Copyright (C) 2022 - 2024  mhahnFr and contributors
  *
  * This file is part of the LeakSanitizer. This library is free software:
  * you can redistribute it and/or modify it under the terms of the
