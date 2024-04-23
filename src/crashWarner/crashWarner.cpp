101070a8e328b06069a3bc141967c6ead5d36ba4
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-04-23 11:05:14 +0200

    Removed the warning and crash helper functions using direct file and line infos

--- src/crashWarner/crashWarner.cpp
+++ src/crashWarner/crashWarner.cpp
@@ -3,18 +3,20 @@
  *
  * Copyright (C) 2023 - 2024  mhahnFr
  *
- * This file is part of the LeakSanitizer. This library is free software:
- * you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version.
+ * This file is part of the LeakSanitizer.
  *
- * This library is distributed in the hope that it will be useful,
+ * The LeakSanitizer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The LeakSanitizer is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with
- * this library, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with the
+ * LeakSanitizer, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include <csignal>
@@ -23,7 +25,6 @@
 #include "crash.hpp"
 #include "warn.hpp"
 
-#include "../LeakSani.hpp"
 #include "../lsanMisc.hpp"
 #include "../formatter.hpp"
 #include "../callstacks/callstackHelper.hpp"
@@ -66,33 +67,6 @@ static inline void printer(const std::string& message, lcs::callstack& callstack
 template<bool Warning>
 static inline void printer(const std::string & message, lcs::callstack && callstack) {
     printer<Warning>(message, callstack);
-}
-
-/**
- * Prints the given message, the file with line number and the given callstack.
- *
- * @param message the message to be printed
- * @param file the file name
- * @param line the line number
- * @param callstack the callstack to be printed
- * @tparam Warning whether to use warning formatting
- */
-template<bool Warning>
-static inline void printer(const std::string & message,
-                           const std::string & file,
-                           const int           line,
-                           lcs::callstack &    callstack) {
-    using formatter::Style;
-    
-    const auto colour = Warning ? Style::MAGENTA : Style::RED;
-    
-    std::cerr << formatter::get<Style::BOLD>
-              << formatter::format<colour>((Warning ? "Warning: " : "") + message) << ", at "
-              << formatter::get<Style::UNDERLINED> << file << ":" << line
-              << formatter::clear<Style::BOLD, Style::UNDERLINED>
-              << std::endl;
-    callstackHelper::format(callstack, std::cerr);
-    std::cerr << std::endl;
 }
 
 /**
@@ -148,15 +122,9 @@ void warn(const std::string & message) {
     });
 }
 
-void warn(const std::string & message, const std::string & file, int line) {
-    withCallstack([&] (auto & callstack) {
-        printer<true>(message, file, line, callstack);
-    });
-}
-
-void warn(const std::string &                                     message,
-          std::optional<std::reference_wrapper<const MallocInfo>> info) {
-    withCallstack([&] (auto & callstack) {
+void warn(const std::string& message,
+          const std::optional<std::reference_wrapper<const MallocInfo>>& info) {
+    withCallstack([&] (auto& callstack) {
         printer<true>(message, info, callstack);
     });
 }
@@ -180,16 +148,9 @@ void crashForce(const std::string& message, const std::optional<std::string>& re
     abort();
 }
 
-void crash(const std::string & message, const std::string & file, int line) {
-    withCallstack([&] (auto & callstack) {
-        printer<false>(message, file, line, callstack);
-        abort();
-    });
-}
-
-void crash(const std::string &                                     message,
-           std::optional<std::reference_wrapper<const MallocInfo>> info) {
-    withCallstack([&] (auto & callstack) {
+void crash(const std::string& message,
+           const std::optional<std::reference_wrapper<const MallocInfo>>& info) {
+    withCallstack([&] (auto& callstack) {
         printer<false>(message, info, callstack);
         abort();
     });
--- src/crashWarner/warn.hpp
+++ src/crashWarner/warn.hpp
@@ -3,18 +3,20 @@
  *
  * Copyright (C) 2023 - 2024  mhahnFr
  *
- * This file is part of the LeakSanitizer. This library is free software:
- * you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version.
+ * This file is part of the LeakSanitizer.
  *
- * This library is distributed in the hope that it will be useful,
+ * The LeakSanitizer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The LeakSanitizer is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with
- * this library, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with the
+ * LeakSanitzer, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef warn_hpp
@@ -23,25 +25,10 @@
 #include <functional>
 #include <optional>
 #include <string>
-#include <utility>
 
 #include "../MallocInfo.hpp"
 
 namespace lsan {
-/**
- * @brief Prints the given message, the file name, the line number and a callstack up
- * to the given omitting address.
- *
- * This function does nothing if the generated callstack is not user relevant.
- *
- * @param message the message to be printed
- * @param file the file name
- * @param line the line number
- */
-void warn(const std::string & message,
-          const std::string & file,
-          const int           line);
-
 /**
  * @brief Prints the given message and a callstack up to the given omitting address.
  *
@@ -61,7 +48,7 @@ void warn(const std::string& message);
  * @param info the optional allocation record
  */
 void warn(const std::string & message,
-          std::optional<std::reference_wrapper<const MallocInfo>> info);
+          const std::optional<std::reference_wrapper<const MallocInfo>>& info);
 }
 
 #endif /* warn_hpp */
