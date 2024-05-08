6e7cee8715530ebf361e0c4d93732f45c0db2a85
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-05-08 20:21:11 +0200

    Added an 'OverAllocator'

---
+++ src/OverAllocator.hpp
@@ -0,0 +1,122 @@
+/*
+ * LeakSanitizer - Small library showing information about lost memory.
+ *
+ * Copyright (C) 2024  mhahnFr
+ *
+ * This file is part of the LeakSanitizer.
+ *
+ * The LeakSanitizer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The LeakSanitizer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with the
+ * LeakSanitizer, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef OverAllocator_hpp
+#define OverAllocator_hpp
+
+#include <new>
+#include <utility>
+
+#include "allocations/realAlloc.hpp"
+
+#define LSAN_OVER_ALLOC 3
+
+namespace lsan {
+namespace overAlloc {
+extern std::size_t amount;
+extern std::pair<std::size_t, void*> allocation;
+extern bool calloc;
+}
+
+template<typename T>
+struct OverAllocator {
+    using value_type = T;
+
+    OverAllocator() = default;
+    template<typename U>
+    OverAllocator(const OverAllocator<U>&) {}
+
+    auto allocate(std::size_t count) -> T* {
+#if LSAN_OVER_ALLOC == 1
+        bool reset = false;
+        std::size_t size = count * sizeof(T) + sizeof(void*);
+        if (count == 1 && overAlloc::amount > 0) {
+            size += overAlloc::amount;
+            reset = true;
+        }
+        void* ptr;
+        if (reset && overAlloc::calloc) {
+            ptr = lsan::real::calloc(size, 1);
+        } else {
+            ptr = lsan::real::malloc(size);
+        }
+        if (ptr == nullptr) {
+            throw std::bad_alloc();
+        }
+        void** addr = reinterpret_cast<void**>(reinterpret_cast<uintptr_t>(ptr) + overAlloc::amount);
+        *addr = ptr;
+        if (reset) {
+            overAlloc::allocation = std::make_pair(overAlloc::amount, ptr);
+            overAlloc::amount = 0;
+            overAlloc::calloc = false;
+        }
+        return reinterpret_cast<T*>(reinterpret_cast<uintptr_t>(ptr) + overAlloc::allocation.first + sizeof(void*));
+#else
+
+        bool reset = false;
+        std::size_t size = count * sizeof(T);
+        if (count == 1 && overAlloc::amount > 0) {
+            size += overAlloc::amount;
+            reset = true;
+        }
+        void* ptr;
+        if (reset && overAlloc::calloc) {
+            ptr = lsan::real::calloc(size, 1);
+        } else {
+            ptr = lsan::real::malloc(size);
+        }
+        if (ptr == nullptr) {
+            throw std::bad_alloc();
+        }
+        if (reset) {
+#if LSAN_OVER_ALLOC == 3
+            overAlloc::allocation = std::make_pair(overAlloc::amount, lsan::real::calloc(overAlloc::amount, 1));
+#else
+            overAlloc::allocation = std::make_pair(overAlloc::amount, reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(ptr) + sizeof(T)));
+#endif
+            overAlloc::amount = 0;
+            overAlloc::calloc = false;
+        }
+        return reinterpret_cast<T*>(ptr);
+#endif
+    }
+
+    void deallocate(T* ptr, std::size_t) {
+#if LSAN_OVER_ALLOC == 1
+        real::free(*reinterpret_cast<void**>(reinterpret_cast<uintptr_t>(ptr) - sizeof(void*)));
+#else
+        real::free(ptr);
+#endif
+    }
+};
+
+template<typename T, typename U>
+inline auto operator==(const OverAllocator<T>&, const OverAllocator<U>&) -> bool {
+    return true;
+}
+
+template<typename T, typename U>
+inline auto operator!=(const OverAllocator<T>& lhs, const OverAllocator<U>& rhs) -> bool {
+    return !(lhs == rhs);
+}
+}
+
+#endif /* OverAllocator_hpp */
