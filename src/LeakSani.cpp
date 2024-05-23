b54285d58112ae5c72d676fbaa0eec6ed8ab2b29
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-05-23 16:10:21 +0200

    Resolved a TODO

--- src/LeakSani.cpp
+++ src/LeakSani.cpp
@@ -671,7 +671,6 @@ auto operator<<(std::ostream& stream, LSan& self) -> std::ostream& {
 
     if ((true)) { // TODO: If should show reachables
         for (const auto& record : stats.recordsGlobal) {
-            // TODO: Actually store them!
             stream << *record << std::endl;
         }
         for (const auto& record : stats.recordsTlv) {
