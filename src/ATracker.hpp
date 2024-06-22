b6d9642c7d18a772026520c62a619a749d4b8f0b
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-06-22 11:00:06 +0000

    Documented the abstract tracker class

--- src/ATracker.hpp
+++ src/ATracker.hpp
@@ -32,19 +32,37 @@
 #include "MallocInfo.hpp"
 
 namespace lsan {
+/**
+ * This class represents the base of an allocation tracker.
+ */
 class ATracker {
 protected:
+    /** The registered allocations.                                   */
     std::map<const void* const, MallocInfo> infos;
+    /** The mutex to manage the access to the registered allocations. */
     std::mutex infoMutex;
 
+    /**
+     * Helper function to potentially add the given allocation record
+     * to the statistics.
+     *
+     * @param info the allocation record
+     */
     virtual inline void addToStats([[ maybe_unused ]] const MallocInfo& info) {}
 
 public:
     virtual ~ATracker() = default;
 
+    /** Indicates whether allocations should be ignored. */
     bool ignoreMalloc = false;
+    /** The mutex to guard allocations in this thread.   */
     std::recursive_mutex mutex;
 
+    /**
+     * Registers the given allocation record.
+     *
+     * @param info the allocation record to be registered
+     */
     inline void addMalloc(MallocInfo&& info) {
         std::lock_guard lock { infoMutex };
         if (__lsan_statsActive) {
@@ -53,14 +71,47 @@ public:
         infos.insert_or_assign(info.pointer, std::move(info));
     }
 
+    /**
+     * Attempts to remove the allocation record for the given pointer.
+     *
+     * @param pointer the pointer of the actual allocation
+     * @return whether the allocation record was removed and the potentially found existing record
+     */
     virtual auto removeMalloc(void* pointer) -> std::pair<const bool, std::optional<MallocInfo::CRef>> = 0;
+
+    /**
+     * Changes the allocation record already registered to the given one.
+     *
+     * @param info the new allocation record
+     */
     virtual void changeMalloc(MallocInfo&& info) = 0;
+
+    /**
+     * Marks this tracker instance as finished, that is, it will ignore all upcoming allocations
+     * and upload it's registered allocation records to the main instance.
+     */
     virtual void finish() = 0;
 
+    /**
+     * @brief Removes the allocation record associated with the given pointer.
+     *
+     * Does not search in other trackers if no record was found.
+     *
+     * @param pointer the allocation pointer
+     * @return whether a record was found and removed
+     */
     virtual auto maybeRemoveMalloc([[ maybe_unused ]] void* pointer) -> bool {
         return false;
     }
 
+    /**
+     * @brief Changes the allocation record already registered to the given one.
+     *
+     * Does not search in other trackers if no record was found.
+     *
+     * @param info the new allocation record
+     * @return whether an allocation record was found and replaced
+     */
     virtual auto maybeChangeMalloc([[ maybe_unused ]] const MallocInfo& info) -> bool {
         return false;
     }
