5e5caf92fbbdb45ed17123f967d018cd8c91a7e7
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-09-28 19:57:08 +0200

    Added auto statistics wrapper class

---
+++ src/statistics/AutoStats.cpp
@@ -0,0 +1,66 @@
+/*
+ * LeakSanitizer - Small library showing information about lost memory.
+ *
+ * Copyright (C) 2024  mhahnFr
+ *
+ * This file is part of the LeakSanitizer.
+ *
+ * The LeakSanitizer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The LeakSanitizer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with the
+ * LeakSanitizer, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <atomic>
+#include <thread>
+
+#include <lsan_stats.h>
+
+#include "../lsanMisc.hpp"
+
+namespace lsan {
+namespace {
+class AutoStats {
+    static std::atomic_bool run;
+
+    std::thread statsThread;
+    bool threadRunning = false;
+
+    static inline void printer() {
+        using namespace std::chrono_literals;
+        while (run) {
+            __lsan_printStats();
+            __lsan_printFStats();
+            std::this_thread::sleep_for(1s);
+        }
+    }
+
+public:
+    inline AutoStats() {
+        if (getBehaviour().autoStatsActive()) {
+            statsThread = std::thread(printer);
+            threadRunning = true;
+        }
+    }
+
+    inline ~AutoStats() {
+        run = false;
+        if (threadRunning) {
+            statsThread.join();
+        }
+    }
+};
+
+std::atomic_bool AutoStats::run = true;
+
+static AutoStats autoStats;
+}
+}
