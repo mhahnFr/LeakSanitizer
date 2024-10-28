0cc32fb29121ae48d222c3a57c75d0993d3a05d6
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-10-28 18:22:30 +0100

    Added a JSON value structure

---
+++ src/suppression/json/Value.hpp
@@ -0,0 +1,40 @@
+/*
+ * LeakSanitizer - Small library showing information about lost memory.
+ *
+ * Copyright (C) 2024  mhahnFr
+ *
+ * This file is part of the LeakSanitizer.
+ *
+ * The LeakSanitizer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The LeakSanitizer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with the
+ * LeakSanitizer, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef Value_hpp
+#define Value_hpp
+
+#include <string>
+#include <variant>
+#include <vector>
+
+namespace lsan::json {
+struct Value {
+    enum class Type {
+        Int, String, Array
+    };
+
+    Type type;
+    std::variant<int, std::vector<Value>, std::string> value;
+};
+}
+
+#endif /* Value_hpp */
