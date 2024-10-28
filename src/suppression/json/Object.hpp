a970f100474f58b0578e198a1b1687ac5fafd470
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-10-28 18:22:52 +0100

    Added a basic JSON object

---
+++ src/suppression/json/Object.hpp
@@ -0,0 +1,45 @@
+/*
+ * LeakSanitizer - Small library showing information about lost memory.
+ *
+ * Copyright (C) 2024  mhahnFr
+ *
+ * This file is part of the LeakSanitizer.
+ *
+ * The LeakSanitizer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The LeakSanitizer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with the
+ * LeakSanitizer, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef Object_hpp
+#define Object_hpp
+
+#include <map>
+#include <optional>
+
+#include "Value.hpp"
+
+namespace lsan::json {
+struct Object {
+    std::map<std::string, Value> content;
+
+    template<typename T>
+    constexpr inline auto get(const std::string& name) -> std::optional<T> {
+        const auto& it = content.find(name);
+        if (it != content.end()) {
+            return get<T>(it->second.value);
+        }
+        return std::nullopt;
+    }
+};
+}
+
+#endif /* Object_hpp */
