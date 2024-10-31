2960c4bc9d64ab4d90a14eb8d288c2f4a6a7180c
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-10-31 18:58:48 +0100

    Added a basic suppression entity skeleton

---
+++ src/suppression/Suppression.cpp
@@ -0,0 +1,36 @@
+/*
+ * LeakSanitizer - Small library showing information about lost memory.
+ *
+ * Copyright (C) 2024  mhahnFr
+ *
+ * This file is part of the LeakSanitizer.
+ *
+ * The LeakSanitizer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The LeakSanitizer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with the
+ * LeakSanitizer, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "Suppression.hpp"
+
+namespace lsan::suppression {
+using namespace json;
+
+Suppression::Suppression(const Object& object):
+    name(object.get<ValueType::String>("name").value_or("<unnamed>")),
+    size(object.get<ValueType::Int>("size"))
+{
+    /*
+     TODO: Parse in the TBD callstack array
+     It is either only the name (string) or an object with the name (string) and the offset (int)
+     */
+}
+}
