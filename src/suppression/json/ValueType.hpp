6f0e836f620d84fb55a12212dbf82ec09ac42757
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-10-31 15:36:17 +0100

    Added dedicated value type enumaration

---
+++ src/suppression/json/ValueType.hpp
@@ -0,0 +1,31 @@
+/*
+ * LeakSanitizer - Small library showing information about lost memory.
+ *
+ * Copyright (C) 2024  mhahnFr
+ *
+ * This file is part of the LeakSanitizer.
+ *
+ * The LeakSanitizer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The LeakSanitizer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with the
+ * LeakSanitizer, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef ValueType_hpp
+#define ValueType_hpp
+
+namespace lsan::json {
+enum class ValueType {
+    Int, String, Array, Bool, Null
+};
+}
+
+#endif /* ValueType_hpp */
