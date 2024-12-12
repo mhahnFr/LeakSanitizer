f51479b33ff0261d33951ed7b5f48c32576ec40e
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-12-12 18:55:14 +0100

    Reworked the behaviour class

--- src/behaviour/Behaviour.hpp
+++ src/behaviour/Behaviour.hpp
@@ -42,45 +42,28 @@ public:\
     }\
 private:
 
-#define FROM_ENV_API(type, name, envName) NEW_ENV(type, name, envName, __lsan_##name)
+#define FROM_ENV_API(type, name, envName) FROM_ENV(type, name, envName, __lsan_##name)
 
     FROM_ENV(bool, suppressionDevelopersMode, SUPPRESSION_DEVELOPER, false)
     FROM_ENV(const char*, suppressionFiles, SUPPRESSION_FILES, nullptr)
 
-    /** Whether to print human-readable.                                 */
-    const std::optional<bool> _humanPrint     = get<bool>("LSAN_HUMAN_PRINT"),
-    /** Whether to print to the standard output stream.                  */
-                              _printCout      = get<bool>("LSAN_PRINT_COUT"),
-    /** Whether to print using ANSI escape codes.                        */
-                              _printFormatted = get<bool>("LSAN_PRINT_FORMATTED"),
-    /** Whether to halt the program when invalid behaviour is detected.  */
-                              _invalidCrash   = get<bool>("LSAN_INVALID_CRASH"),
-    /** Whether to check for invalid `free`s.                            */
-                              _invalidFree    = get<bool>("LSAN_INVALID_FREE"),
-    /** Whether to check for `free`ing `NULL`.                           */
-                              _freeNull       = get<bool>("LSAN_FREE_NULL"),
-    /** Whether to check for zero allocations.                           */
-                              _zeroAllocation = get<bool>("LSAN_ZERO_ALLOCATION"),
+    FROM_ENV_API(bool, humanPrint,     HUMAN_PRINT)
+    FROM_ENV_API(bool, printCout,      PRINT_COUT)
+    FROM_ENV_API(bool, printFormatted, PRINT_FORMATTED)
+    FROM_ENV_API(bool, invalidCrash,   INVALID_CRASH)
+    FROM_ENV_API(bool, invalidFree,    INVALID_FREE)
+    FROM_ENV_API(bool, freeNull,       FREE_NULL)
+    FROM_ENV_API(bool, zeroAllocation, ZERO_ALLOCATION)
+    FROM_ENV_API(bool, printExitPoint, PRINT_EXIT_POINT)
+    FROM_ENV_API(bool, printBinaries,  PRINT_BINARIES)
+    FROM_ENV_API(bool, printFunctions, PRINT_FUNCTIONS)
+    FROM_ENV_API(bool, relativePaths,  RELATIVE_PATHS)
+
     /** Whether to activate the statistical bookkeeping.                 */
-                              _statsActive    = get<bool>("LSAN_STATS_ACTIVE"),
-    /** Whether to print the callstack of the exit point.                */
-                              _printExitPoint = get<bool>("LSAN_PRINT_EXIT_POINT"),
-    /** Whether to print the names of the binary files.                  */
-                              _printBinaries  = get<bool>("LSAN_PRINT_BINARIES"),
-    /** Whether to print function names when line numbers are available. */
-                              _printFunctions = get<bool>("LSAN_PRINT_FUNCTIONS"),
-    /** Whether to relativate paths.                                     */
-                              _relativePaths  = get<bool>("LSAN_RELATIVE_PATHS");
+    const std::optional<bool> _statsActive = get<bool>("LSAN_STATS_ACTIVE");
 
-    /** The amount of leaks to print.                                    */
-    const std::optional<std::size_t> _leakCount           = get<std::size_t>("LSAN_LEAK_COUNT"),
-    /** The amount of frames to print in callstacks.                     */
-                                     _callstackSize       = get<std::size_t>("LSAN_CALLSTACK_SIZE"),
-    /** The threshold for callstacks to be treated as user-relevant.     */
-                                     _firstPartyThreshold = get<std::size_t>("LSAN_FIRST_PARTY_THRESHOLD");
-
-    /** The regex to detect first party binary names.                    */
-    const std::optional<const char*> _firstPartyRegex = getVariable("LSAN_FIRST_PARTY_REGEX");
+    FROM_ENV_API(std::size_t, leakCount, LEAK_COUNT)
+    FROM_ENV_API(std::size_t, callstackSize, CALLSTACK_SIZE)
 
     /** The time interval between the automatical statistics printing.   */
     const std::optional<std::chrono::nanoseconds> _autoStats = get<std::chrono::nanoseconds>("LSAN_AUTO_STATS");
@@ -95,27 +78,7 @@ private:
         return _statsActive ? *_statsActive : __lsan_statsActive;
     }
 
-#define ENV_OR_API(name)                       \
-inline auto name() const {                     \
-    return _##name ? *_##name : __lsan_##name; \
-}
-
 public:
-    ENV_OR_API(humanPrint)
-    ENV_OR_API(printCout)
-    ENV_OR_API(printFormatted)
-    ENV_OR_API(invalidCrash)
-    ENV_OR_API(invalidFree)
-    ENV_OR_API(freeNull)
-    ENV_OR_API(zeroAllocation)
-    ENV_OR_API(printExitPoint)
-    ENV_OR_API(printBinaries)
-    ENV_OR_API(printFunctions)
-    ENV_OR_API(relativePaths)
-
-    ENV_OR_API(leakCount)
-    ENV_OR_API(callstackSize)
-
     /**
      * Returns whether the stats should be active.
      *
@@ -134,8 +97,8 @@ public:
         return _autoStats;
     }
 
-#undef ENV_OR_API
-#undef NEW_ENV
+#undef FROM_ENV
+#undef FROM_ENV_API
 };
 }
 
