7a3b4042871a075b8314f8fc453b738167045964
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2025-01-24 18:13:26 +0100

    Added a thread info

---
+++ src/ThreadInfo.hpp
@@ -0,0 +1,40 @@
+/*
+ * LeakSanitizer - Small library showing information about lost memory.
+ *
+ * Copyright (C) 2025  mhahnFr
+ *
+ * This file is part of the LeakSanitizer.
+ *
+ * The LeakSanitizer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The LeakSanitizer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with the
+ * LeakSanitizer, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef ThreadInfo_hpp
+#define ThreadInfo_hpp
+
+#include <optional>
+#include <thread>
+
+#include <pthread.h>
+
+namespace lsan {
+struct ThreadInfo {
+    std::thread::id id;
+
+    pthread_t thread;
+
+    std::optional<void*> beginFrameAddress;
+};
+}
+
+#endif /* ThreadInfo_hpp */
