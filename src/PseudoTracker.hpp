03fb4237dbdae067f8c7f95868dd87af5ae5eb81
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2025-02-07 11:50:22 +0100

    Added a pseudo thread local tracker

---
+++ src/PseudoTracker.hpp
@@ -0,0 +1,67 @@
+/*
+ * LeakSanitizer - Small library showing information about lost memory.
+ *
+ * Copyright (C) 2025  mhahnFr
+ *
+ * This file is part of the LeakSanitizer.
+ *
+ * The LeakSanitizer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The LeakSanitizer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with the
+ * LeakSanitizer, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef PseudoTracker_hpp
+#define PseudoTracker_hpp
+
+#include "ATracker.hpp"
+#include "lsanMisc.hpp"
+
+namespace lsan {
+class PseudoTracker final: public ATracker {
+    LSan& main = getInstance();
+
+protected:
+    virtual inline void maybeAddToStats(const MallocInfo& info) override {
+        infos.erase(info.pointer);
+        main.addMalloc(MallocInfo(info));
+    }
+
+public:
+    inline PseudoTracker() {
+        main.registerTracker(this);
+    }
+
+    virtual inline ~PseudoTracker() {
+        main.deregisterTracker(this);
+    }
+
+    virtual inline auto removeMalloc(void* pointer) -> std::pair<bool, std::optional<MallocInfo::CRef>> override {
+        return main.removeMalloc(pointer);
+    }
+
+    virtual inline void changeMalloc(MallocInfo&& info) override {
+        main.changeMalloc(std::move(info));
+    }
+
+    virtual inline void finish() override {}
+
+    virtual inline auto maybeRemoveMalloc(void* pointer) -> std::pair<bool, std::optional<MallocInfo::CRef>> override {
+        return main.maybeRemoveMalloc(pointer);
+    }
+
+    virtual inline auto maybeChangeMalloc(const MallocInfo& info) -> bool override {
+        return main.maybeChangeMalloc(info);
+    }
+};
+}
+
+#endif /* PseudoTracker_hpp */
