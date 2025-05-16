a97751e44d6a9a24db3d2fe5cb06ca6d89793dd7
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2025-05-16 11:51:07 +0200

    Various minor code adjustments to the RealAllocator

--- src/allocators/RealAllocator.hpp
+++ src/allocators/RealAllocator.hpp
@@ -1,7 +1,7 @@
 /*
  * LeakSanitizer - Small library showing information about lost memory.
  *
- * Copyright (C) 2024  mhahnFr
+ * Copyright (C) 2024 - 2025  mhahnFr
  *
  * This file is part of the LeakSanitizer.
  *
@@ -43,16 +43,16 @@ struct RealAllocator
     RealAllocator() = default;
 
     template<typename U>
-    constexpr inline RealAllocator(const RealAllocator<U>&) noexcept {}
+    explicit constexpr RealAllocator(const RealAllocator<U>&) noexcept {}
 
     /**
      * Allocates and returns a block of memory fitting for the given amount of objects.
      *
      * @param n the amount of objects to be allocated
      * @return the allocated block
-     * @throws if too many objects are requested or when the allocator failed to allocate
+     * @throws std::bad_alloc if too many objects are requested or when the allocator failed to allocate
      */
-    [[ nodiscard ]] constexpr inline auto allocate(std::size_t n) -> T* {
+    [[ nodiscard ]] constexpr auto allocate(const std::size_t n) -> T* {
         if (n > std::numeric_limits<std::size_t>::max() / sizeof(T)) {
             throw std::bad_array_new_length();
         }
@@ -71,17 +71,17 @@ struct RealAllocator
      *
      * @param p the pointer to be deallocated
      */
-    constexpr inline void deallocate(T* p, std::size_t) noexcept {
+    constexpr void deallocate(T* p, std::size_t) noexcept {
         real::free(p);
     }
 
     template<typename U>
-    constexpr inline auto operator==(const RealAllocator<U>&) const noexcept -> bool {
+    constexpr auto operator==(const RealAllocator<U>&) const noexcept -> bool {
         return true;
     }
 
     template<typename U>
-    constexpr inline auto operator!=(const RealAllocator<U>&) const noexcept -> bool {
+    constexpr auto operator!=(const RealAllocator<U>&) const noexcept -> bool {
         return true;
     }
 };
