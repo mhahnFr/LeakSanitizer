ea6005f48848b8baee8b1d3df3a3098a9d9a7dd6
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2025-07-29 11:37:35 +0200

    Added CFBundle management for macOS

---
+++ src/macos/bundle.hpp
@@ -0,0 +1,32 @@
+/*
+ * LeakSanitizer - Small library showing information about lost memory.
+ *
+ * Copyright (C) 2025  mhahnFr
+ *
+ * This file is part of the LeakSanitizer.
+ *
+ * The LeakSanitizer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The LeakSanitizer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with the
+ * LeakSanitizer, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef bundle_hpp
+#define bundle_hpp
+
+#include <CoreFoundation/CFBundle.h>
+
+namespace lsan::macos::bundle {
+auto getBundle() -> CFBundleRef;
+void killBundle();
+}
+
+#endif /* bundle_hpp */
