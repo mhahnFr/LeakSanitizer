8f729247efb3e7a6a42aa20fa4aeb13824fc515a
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2025-09-08 09:44:19 +0200

    Implemented a dedicated behaviour getter

---
+++ src/behaviour/getBehaviour.hpp
@@ -0,0 +1,31 @@
+/*
+ * LeakSanitizer - Small library showing information about lost memory.
+ *
+ * Copyright (C) 2025  mhahnFr
+ *
+ * This file is part of the LeakSanitizer.
+ *
+ * The LeakSanitizer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The LeakSanitizer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with the
+ * LeakSanitizer, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef getBehaviour_hpp
+#define getBehaviour_hpp
+
+#include "Behaviour.hpp"
+
+namespace lsan::behaviour {
+auto getBehaviour() -> const Behaviour&;
+}
+
+#endif /* getBehaviour_hpp */
