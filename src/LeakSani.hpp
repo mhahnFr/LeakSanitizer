5b5e58434d296c35481347c87a62542198bddee6
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2025-09-29 15:06:36 +0200

    Documented a new field

--- src/LeakSani.hpp
+++ src/LeakSani.hpp
@@ -80,6 +80,7 @@ class LSan final: public trackers::ATracker {
     const std::thread::id mainId = std::this_thread::get_id();
     /** The thread-local storage key used for the thread-local allocation trackers.     */
     const pthread_key_t saniKey;
+    /** The alternative signal handling execution stack.                                */
     void* const signalStack;
     
 #ifdef BENCHMARK
